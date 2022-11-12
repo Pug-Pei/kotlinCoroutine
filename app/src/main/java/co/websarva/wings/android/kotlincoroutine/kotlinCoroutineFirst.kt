0b9112cc4ca42b2d3f89b4d4e1186dcb3a4b4603
kouhei <grimpoteuthistako@gmail.com>
2022-11-12 14:14:00 +0900

    Initial commit

---
+++ app/src/main/java/co/websarva/wings/android/kotlincoroutine/kotlinCoroutineFirst.kt
@@ -0,0 +1,21 @@
+package co.websarva.wings.android.kotlincoroutine
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+
+    println("runBlocking: $this")
+
+    launch {
+        println("launch: $this")
+
+        launch {
+            println("child launch: $this")
+        }
+    }
+
+    async {
+        println("async: $this")
+    }
+}
---
+++ app/src/main/java/co/websarva/wings/android/kotlincoroutine/kotlinCoroutineSecond.kt
@@ -0,0 +1,2 @@
+package co.websarva.wings.android.kotlincoroutine
+
