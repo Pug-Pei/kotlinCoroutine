af34b7aacc7edabc28f28cac74d1bc6dc35b18b0
Pug-Pei <grimpoteuthistako@gmail.com>
2022-11-21 22:18:35 +0900

    サンプルコードを追加

--- app/src/main/java/co/websarva/wings/android/kotlincoroutine/kotlinCoroutine.kt
+++ app/src/main/java/co/websarva/wings/android/kotlincoroutine/kotlinCoroutine.kt
@@ -1,7 +1,28 @@
 package co.websarva.wings.android.kotlincoroutine
 
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
-fun main() = {
 
+fun main() = runBlocking {    // Creates a blocking coroutine that executes in current thread (main)
+
+    println("Main program starts: ${Thread.currentThread().name}")  // main thread
+
+    val msgOne: Deferred<String> = async (start= CoroutineStart.LAZY){ getMessageOne()}
+    val msgTwo: Deferred<String> = async (start= CoroutineStart.LAZY){ getMessageTwo() }
+    println("The entire message is: ${msgOne.await() + msgTwo.await()}")
+
+    println("Main program ends: ${Thread.currentThread().name}")    // main thread
 }
 
+suspend fun getMessageOne(): String {
+    delay(1000L)    // pretend to do some work
+    println("After working in getMessageOne()")
+    return "Hello "
+}
+
+suspend fun getMessageTwo(): String {
+    delay(1000L)    // pretend to do some work
+    println("After working in getMessageTwo()")
+    return "World!"
+}
